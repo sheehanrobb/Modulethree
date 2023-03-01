2c2b5252af6ecd9885564205bf041ca25a6f91dc
Sheehan Robb <sheehanrobb01@gmail.com>
2023-03-01 14:06:12 +1300

    first exercise

---
+++ fundamentals.js
@@ -0,0 +1,32 @@
+// Section 1
+// console.log('"" + 1 + 0')//10
+// console.log('"" - 1 + 0'); //-1 + 0
+// console.log('true + false'); // 1
+// console.log('!true'); // false
+// console.log('6 / "3"'); // 2
+// console.log('"2" * "3" ');// 6
+// console.log(''); 4 + 5 + "px"//9px
+// console.log('"$" + 4 + 5'); //$45
+// console.log('"4" - 2'); // 2
+// console.log('"4px" - 2'); //2px
+// console.log('" -9 " + 5'); //-4
+// console.log('" -9 " - 5'); //-14
+// console.log('null + 1'); // 1
+// console.log('undefined + 1'); // false
+// console.log('undefined == null'); // true
+// console.log('undefined === null'); //false
+// console.log('" \t \n" - 2"');
+
+//Section2 
+
+let three = "3"
+let four = "4"
+let thirty = "30"
+//what is the value of the following expressions?
+
+let addition = three + four
+let multiplication = three * four
+let division = three / four
+let subtraction = three - four
+let lessThan1 = three < four
+let lessThan2 = thirty < four
