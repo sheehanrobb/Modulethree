5814d1310e6d991c3071b6bf40d800d2f797424e
Sheehan Robb <sheehanrobb01@gmail.com>
2023-03-04 08:52:02 +1300

    Q 6

--- fundamentals.js
+++ fundamentals.js
@@ -1,38 +1,42 @@
 // Section 1
-// console.log('"" + 1 + 0') 10 as the + performs string concatenation
-// console.log('"" - 1 + 0'); -1 as operator performs arithmetic and empty string is converted to 0
-// console.log('true + false');  1 in js true = 1 and false = 0
-// console.log('!true');  false
-// console.log('6 / "3"');  2 "3'(string) is converted 3 (number) 
-// console.log('"2" * "3" ');// 6 both strings are converted to numbers
-// console.log(' 4 + 5 + "px"')//"9px" artithmetic then conversion and concatenation with a string
-// console.log('"$" + 4 + 5'); //"$45" string concatenation
-// console.log('"4" - 2'); // 2
-// console.log('"4px" - 2'); //NaN as 4px can't be converted to a number
-// console.log('" -9 " + 5'); //"-95" string concatenation
-// console.log('" -9 " - 5'); //-14 string is converted to number and equation can be completed
-// console.log('null + 1'); // 1 null is treated as 0 in arithmetic operations
-// console.log('undefined + 1'); // NaN- undefined can't be converted to a number
-// console.log('undefined == null'); // true both null and undefine in js are considered = to each other but not any other value
-// console.log('undefined === null'); //false these are not considered exactly equal
-// console.log('" \t \n" - 2"'); string is considered and empty string and therefore can be converted to number 0
+// console.log("" + 1 + 0) 10 as the + performs string concatenation
+// console.log("" - 1 + 0); -1 as operator performs arithmetic and empty string is converted to 0
+// console.log(true + false);  1 in js true = 1 and false = 0
+// console.log(!true);  false
+// console.log(6 / "3");  2 "3'(string) is converted 3 (number) 
+// console.log("2" * "3" );// 6 both strings are converted to numbers
+// console.log( 4 + 5 + "px")//"9px" artithmetic then conversion and concatenation with a string
+// console.log("$" + 4 + 5); //"$45" string concatenation
+// console.log("4" - 2); // 2
+// console.log("4px" - 2); //NaN as 4px can't be converted to a number
+// console.log(" -9 " + 5); //"-95" string concatenation
+// console.log(" -9 " - 5); //-14 string is converted to number and equation can be completed
+// console.log(null + 1); // 1 null is treated as 0 in arithmetic operations
+// console.log'undefined + 1); // NaN- undefined can't be converted to a number
+// console.log(undefined == null); // true both null and undefine in js are considered = to each other but not any other value
+// console.log(undefined === null); //false these are not considered exactly equal
+// console.log(" \t \n" - 2");-2 string is considered and empty string and therefore can be converted to number 0
 
-/*//Section2 
-
+//Section2 
+/*
 let three = "3"
 let four = "4"
 let thirty = "30"
 //what is the value of the following expressions?
 
-let addition = three + four //"34" concatenation of strings
-// 'Nan' you can't multiply, divide, or subtract strings
-let multiplication = three * four//
-let division = three / four
+let addition = three + four //34 (concatenation)
+console.log(addition) 
+let multiplication = three * four//12
+console.log(multiplication)
+let division = three / four//0.75
+console.log(division);
 let subtraction = three - four
-// to fix you need to use the number function ' number (three) * number (four) '
+console.log(subtraction); -1
 
-let lessThan1 = three < four // true correct  if doing numerically
-let lessThan2 = thirty < four// false if doing numerically, needs to be converted from stringsto nuumbers
+let lessThan1 = three < four // True
+console.log(lessThan1) 
+let lessThan2 = thirty < four//Truelexigraphical order so its comparing the 3 in thirty to the 4  
+console.log(lessThan2)
 */
 
 /* Q3- Which of the following console.log messages will print? Why?
@@ -42,7 +46,105 @@ if (null) console.log('null is true') won't print
 if (-1) console.log('negative is true')will print
 if (1) console.log('positive is true') will print 
 
-reasons: 0, null, undefined empty String, false and NaN are considered falsy and will not execute  in an if statement. 
+reasons: 0, null, undefined, empty String, false and NaN are considered falsy and will not execute  in an if statement. 
 any other value is considered truthy and will execute in the if statements
 */
 
+/*
+let a = 2, b = 3;
+let result = `${a} + ${b} is `;
+console.log(result)
+if (a + b < 10) ==> (a,b) {
+result += 'less than 10';
+} else {
+result += 'greater than 10';
+}
+*/
+// rewritten
+
+//let a = 2, b = 3;
+//let result = `${a} + ${b} is`  + ((a + b < 10) ? 'less than 10' : 'greater than 10');
+
+
+
+/*Q5 
+
+5. Rewrite the following function using: a) function expression syntax, and b) arrow function
+syntax. Test each version to make sure they work the same.
+
+function getGreeting(name) {
+return 'Hello ' + name + '!';
+}
+
+//a) function expression
+const getGreeting = function(name) {
+    return `Hello ${name}!`;
+}
+console.log(getGreeting('Sheehan'));
+
+or
+
+const getGreeting = function(name) {
+    return ('Hello ' + name + ' !');
+}
+console.log(getGreeting('Sheehan'));
+
+//b) arrow
+const getGreeting = (name) => ('Hello ' + name + " !");
+console.log(getGreeting('Sheehan'));
+
+or
+
+const getGreeting = (name) => (`Hello ${name} !`)
+console.log(getGreeting('Sheehan'));
+*/
+
+//Q6
+//A and B
+/*
+const westley = {
+    name: 'Westley',
+    numFingers: 5
+ }
+const rugen = {
+    name: 'Count Rugen',
+    numFingers: 6
+}
+const inigo = {
+    firstName: 'Inigo',
+    lastName: 'Montoya',
+    greeting(person) => {
+    let greeting = `Hello ${person.name}, my name is ${this.firstName} ${this.lastName}. `
+    console.log(greeting + this.getCatchPhrase(person));
+    }
+},
+   
+    getCatchPhrase(person) => {
+        return person.numFingers === 6 ? "You killed my father, Prepare to Die!" : "Nice to meet you";
+    }
+    
+}
+
+    inigo.greeting(westley)
+    inigo.greeting(rugen)
+*/
+
+   /* Q7
+   const basketballGame = {
+    score: 0,
+    freeThrow() {
+    this.score++;
+    },
+    basket() {
+    this.score += 2;
+    },
+    threePointer() {
+    this.score += 3;
+    },
+    halfTime() {
+    console.log('Halftime score is '+this.score);
+    }
+    }
+    //modify each of the above object methods to enable function chaining as below:
+    basketballGame.basket().freeThrow().freeThrow().basket().threePointer().halfTime();
+    */
---
+++ intermediate.js
@@ -0,0 +1,14 @@
+
+/*const getGreeting = function(name) {
+    alert('Hello' + name + '!');
+}
+*/
+
+/*sayHi('John');
+function sayHi (name) {
+    alert('Hello, ${name}');
+}
+*/
+function getGreeting(name) {
+    return 'Hello ' + name + '!';
+    }
